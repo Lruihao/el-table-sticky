84d4bb57999186186cda69bedf57e59d3d40649f
Cell <1024@lruihao.cn>
2023-06-15 14:55:59 +0800

    Chore: 修改检查到 views/*.vue 文件改动自动生成路由配置

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '*.{js,jsx,vue}': 'vue-cli-service lint',
-  'src/router.js': 'npm run gr && git add src/router.js',
+  'src/views/*.{vue}': 'npm run gr && git add src/router.js',
 }
