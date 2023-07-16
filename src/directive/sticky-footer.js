eae9fe3c17e5d0ebc80bbe96a429582964e38285
Cell <1024@lruihao.cn>
2023-07-16 19:53:00 +0800

    Fix: 修复打包后，new.target.name 变化导致的 bug

--- src/directive/sticky-footer.js
+++ src/directive/sticky-footer.js
@@ -2,6 +2,7 @@ import Sticky from '@/utils/sticky'
 import './css/sticky-footer.scss'
 
 export default class StickyFooter extends Sticky {
+  static name = 'StickyFooter'
   constructor(...args) {
     super(...args)
   }
--- src/directive/sticky-header.js
+++ src/directive/sticky-header.js
@@ -2,6 +2,7 @@ import Sticky from '@/utils/sticky'
 import './css/sticky-header.scss'
 
 export default class StickyHeader extends Sticky {
+  static name = 'StickyHeader'
   constructor(...args) {
     super(...args)
   }
