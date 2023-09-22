2e807e516ec41b5815235a669d055fc126eead89
Cell <1024@lruihao.cn>
2023-09-22 12:18:41 +0800

    Refactor: refactor function Sticky.#stackStickyColumns to reduce its Cognitive Complexity from 23 to the 15 allowed

--- src/utils/sticky.js
+++ src/utils/sticky.js
@@ -31,71 +31,83 @@ export default class Sticky {
   }
 
   /**
+   * Stack sticky for left fixed columns
+   * @param {Array} tableCell table header or footer cells
+   */
+  #stackLeftColumns(tableCell = []) {
+    let stickyLeft = 0
+    for (let i = 0; i < tableCell.length; i++) {
+      const th = tableCell[i]
+      if (th.classList.contains('is-hidden')) {
+        th.dataset.sticky = 'left'
+        th.style.left = `${stickyLeft}px`
+        stickyLeft += th.offsetWidth
+        // set data-sticky-left-last attribute for last left fixed column
+        if (!th.nextElementSibling?.classList.contains('is-hidden')) {
+          th.dataset.sticky = 'end'
+          break
+        }
+      } else if (i === 0) {
+        // no left fixed columns
+        break
+      }
+    }
+  }
+
+  /**
+   * Stack sticky for right fixed columns
+   * @param {Array} tableCell table header or footer cells
+   */
+  #stackRightColumns(tableCell = []) {
+    let stickyRight = 0
+    for (let i = tableCell.length - 1; i >= 0; i--) {
+      const th = tableCell[i]
+      if (th.classList.contains('is-hidden') && !th.dataset.sticky) {
+        th.dataset.sticky = 'right'
+        th.style.right = `${stickyRight}px`
+        stickyRight += th.offsetWidth
+        // set data-sticky-right-first attribute for first right fixed column
+        if (!th.previousElementSibling?.classList.contains('is-hidden')) {
+          th.dataset.sticky = 'start'
+          break
+        }
+      }
+    }
+  }
+
+  /**
    * Stack sticky left and right columns for el-table header or footer
    * @param {Element} el el-table element
    * @param {Object} binding binding
    * @param {Object} vnode vnode
    * @private
    */
-  #stackStickyColumns(el, binding, vnode) {
+  async #stackStickyColumns(el, binding, vnode) {
     const { componentInstance: $table } = vnode
     const { value } = binding
 
     // wait for el-table render
-    // TODO Refactor this function to reduce its Cognitive Complexity from 23 to the 15 allowed.
-    $table.$nextTick(() => {
-      let tableStickyWrapper
-      let tableCell
+    await $table.$nextTick()
 
+    let tableStickyWrapper
+    let tableCell
+
+    if (this.#target === 'StickyHeader') {
       // for sticky header
-      if (this.#target === 'StickyHeader') {
-        tableStickyWrapper = el.querySelector('.el-table__header-wrapper')
-        tableStickyWrapper.style.top = value?.offsetTop ? convertToPx(value.offsetTop) : this.offsetTop
-        tableCell = tableStickyWrapper.querySelectorAll('.el-table__header .el-table__cell')
-      }
+      tableStickyWrapper = el.querySelector('.el-table__header-wrapper')
+      tableStickyWrapper.style.top = value?.offsetTop ? convertToPx(value.offsetTop) : this.offsetTop
+      tableCell = tableStickyWrapper.querySelectorAll('.el-table__header .el-table__cell')
+    }
+    if (this.#target === 'StickyFooter') {
       // for sticky footer
-      if (this.#target === 'StickyFooter') {
-        tableStickyWrapper = el.querySelector('.el-table__footer-wrapper')
-        tableStickyWrapper.style.bottom = value?.offsetBottom ? convertToPx(value.offsetBottom) : this.offsetBottom
-        tableCell = tableStickyWrapper.querySelectorAll('.el-table__footer .el-table__cell')
-      }
-      if (el.querySelector('.is-scrolling-none')) return
+      tableStickyWrapper = el.querySelector('.el-table__footer-wrapper')
+      tableStickyWrapper.style.bottom = value?.offsetBottom ? convertToPx(value.offsetBottom) : this.offsetBottom
+      tableCell = tableStickyWrapper.querySelectorAll('.el-table__footer .el-table__cell')
+    }
+    if (el.querySelector('.is-scrolling-none')) return
 
-      let stickyLeft = 0
-      let stickyRight = 0
-
-      // stack sticky for left fixed columns
-      for (let i = 0; i < tableCell.length; i++) {
-        const th = tableCell[i]
-        if (th.classList.contains('is-hidden')) {
-          th.dataset.sticky = 'left'
-          th.style.left = `${stickyLeft}px`
-          stickyLeft += th.offsetWidth
-          // set data-sticky-left-last attribute for last left fixed column
-          if (!th.nextElementSibling?.classList.contains('is-hidden')) {
-            th.dataset.sticky = 'end'
-            break
-          }
-        } else if (i === 0) {
-          // no left fixed columns
-          break
-        }
-      }
-      // stack sticky for right fixed columns
-      for (let i = tableCell.length - 1; i >= 0; i--) {
-        const th = tableCell[i]
-        if (th.classList.contains('is-hidden') && !th.dataset.sticky) {
-          th.dataset.sticky = 'right'
-          th.style.right = `${stickyRight}px`
-          stickyRight += th.offsetWidth
-          // set data-sticky-right-first attribute for first right fixed column
-          if (!th.previousElementSibling?.classList.contains('is-hidden')) {
-            th.dataset.sticky = 'start'
-            break
-          }
-        }
-      }
-    })
+    this.#stackLeftColumns(tableCell)
+    this.#stackRightColumns(tableCell)
   }
 
   /**
