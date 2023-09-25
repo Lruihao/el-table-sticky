32fb50718886f269907271c018b07885d3b69fbb
Cell <1024@lruihao.cn>
2023-09-25 12:24:10 +0800

    Chore(deps-dev): add nullish-coalescing-operator ployfill

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ module.exports = {
     '@vue/cli-plugin-babel/preset',
   ],
   plugins: [
-    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-private-methods',
   ],
 }
